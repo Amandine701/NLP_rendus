1e37a1fc0eb9cb40a6d181db3ce7f121cf74e288
aperret <amandine.perret@ensae.fr>
2026-02-13 09:19:34 +0000

    Création du df nettoyé, filtré et unifié

---
+++ create_dataframe.py
@@ -0,0 +1,40 @@
+# This file creates a unified and cleaned dataframe based on the results of the descriptive statistical analysis.
+
+import os
+import pandas as pd
+
+def prepare_legislatives_dataset(csv_path, txt_folder_path, threshold=30):
+    """
+    This function creates a unified, cleaned and exploitable dataframe
+    """
+    
+    ## DATA LOADING & INITIAL CLEANING 
+    # Load original dataset
+    csv = pd.read_csv(csv_path, encoding="utf-8")
+
+    # Select relevant columns and drop rows with invalid 'titulaire-soutien' labels
+    df_reduced = csv[["id", "contexte-tour", "titulaire-soutien"]].copy()
+    df_valid = df_reduced[~(df_reduced['titulaire-soutien'].isna() | 
+                    (df_reduced['titulaire-soutien'] == '') | 
+                    (df_reduced['titulaire-soutien'] == 'non mentionné'))]
+
+    ##  TEXT CONTENT INTEGRATION 
+    path_to_files = txt_folder_path
+    txt_files = [f for f in os.listdir(path_to_files) if f.endswith(".txt")]
+
+    # Extract ID and text content from files
+    file_data = [{"id": f.replace(".txt", ""), "text_content": open(os.path.join(path_to_files, f), 'r', encoding='utf-8').read()} 
+                 for f in txt_files]
+
+    # Merge metadata with text content
+    df_txt = pd.DataFrame(file_data)
+    df_valid = pd.merge(df_valid, df_txt, on="id", how="left")
+
+    ##  FREQUENCY FILTERING (N >= 30) 
+    # Define threshold for statistical significance
+    SEUIL_MIN = threshold
+    df_filtered = df_valid.groupby("titulaire-soutien").filter(lambda x: len(x) >= SEUIL_MIN).copy()
+    df_filtered = df_filtered.reset_index(drop=True)
+
+    # Update working dataframe for downstream tasks (splits, training)
+    return df_filtered
