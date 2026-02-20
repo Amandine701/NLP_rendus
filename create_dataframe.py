03823036f9114e78962e9adf423059dd13b7464a
aperret <amandine.perret@ensae.fr>
2026-02-20 21:02:51 +0000

    nouvelle fonction pour lisibilité sur ensemble des manifestes

--- create_dataframe.py
+++ create_dataframe.py
@@ -104,3 +104,36 @@ def prepare_legislatives_dataset_without_bi_categories(csv_path, txt_folder_path
 
     # Update working dataframe for downstream tasks (splits, training)
     return df_filtered
+
+def prepare_legislatives_dataset_lisibilite(csv_path, txt_folder_path, threshold=30):
+    import pandas as pd
+    import os
+    """
+    This function is the same as prepare_legislatives_dataset except we don't 
+    remove manifestos without identified support and parties with less than 30 manifestos
+    """
+    
+    ## DATA LOADING & INITIAL CLEANING 
+    # Load original dataset
+    csv = pd.read_csv(csv_path, encoding="utf-8")
+
+    # Select relevant columns and drop rows with invalid 'titulaire-soutien' labels
+    df_reduced = csv[["id", "contexte-tour", "titulaire-soutien", "date"]].copy()
+    df_valid = df_reduced[(df_reduced['contexte-tour'] == 1)
+        ].copy()
+
+
+    ##  TEXT CONTENT INTEGRATION 
+    path_to_files = txt_folder_path
+    txt_files = [f for f in os.listdir(path_to_files) if f.endswith(".txt")]
+
+    # Extract ID and text content from files
+    file_data = [{"id": f.replace(".txt", ""), "text_content": open(os.path.join(path_to_files, f), 'r', encoding='utf-8').read()} 
+                 for f in txt_files]
+
+    # Merge metadata with text content
+    df_txt = pd.DataFrame(file_data)
+    df_valid = pd.merge(df_valid, df_txt, on="id", how="left")
+
+    # Df filtered
+    return df_filtered
